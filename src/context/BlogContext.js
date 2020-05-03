14b793cf98ca9d515601bc8e09ca231047a818af
Victor Iris <victormidp@gmail.com>
2020-05-02 22:41:55 -0500

    Add api support to delete post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -38,7 +38,8 @@ const editBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-	return (id) => {
+	return async (id) => {
+		await jsonServer.delete(`/blogposts/${id}`);
 		dispatch({ type: "delete_blogpost", payload: id });
 	};
 };
